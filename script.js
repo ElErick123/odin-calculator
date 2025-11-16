e0ae83d6de0a73ca5b0d1978877ed81f46636771
Erick <erickcarocravioto@gmail.com>
2025-11-16 15:46:29 -0600

    Add eventListeners for operator buttons, making you not able to put 2 operators together

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 const display = document.getElementById("display-screen");
-const buttons = document.querySelectorAll("button");
+const numberButtons = document.querySelectorAll(".black-button");
+const operatorButtons = document.querySelectorAll(".orange-button");
+const operators = ["+", "-", "x", "/", "%"];
 
-buttons.forEach((button) => {
+numberButtons.forEach((button) => {
     button.addEventListener("click", (e) => {
-        if (isNaN(e.target.textContent)) {
-            return
-        }
         if (display.textContent === "0") {
             display.textContent = e.target.textContent;
         }
         else {
             display.textContent += e.target.textContent;
         }
+    });
+});
+
+operatorButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        const lastDigit = display.textContent.slice(display.textContent.length - 1, display.textContent.length);
+        console.log(lastDigit);
+        if (operators.includes(lastDigit)) {
+            return
+        }
+        display.textContent += e.target.textContent;
     });
 });
 
