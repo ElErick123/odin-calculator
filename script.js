acc19d386bcada11b868767c0c35f0742213e1b7
Erick <erickcarocravioto@gmail.com>
2025-11-15 23:03:35 -0600

    Added event listeners to the buttons to add numbers to the screen

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+const display = document.getElementById("display-screen");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        if (isNaN(e.target.textContent)) {
+            return
+        }
+        if (display.textContent === "0") {
+            display.textContent = e.target.textContent;
+        }
+        else {
+            display.textContent += e.target.textContent;
+        }
+    });
+});
+
 function add(a, b) {
     return a + b;
 }
